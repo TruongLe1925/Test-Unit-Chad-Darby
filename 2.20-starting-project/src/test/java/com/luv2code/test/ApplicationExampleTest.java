3cdf5459ec45308f26ad2a80d454aa49fab1aa1d
TruongLe1925 <lephuoctruongppp@gmail.com>
2026-04-24 23:37:13 +0700

    update:mock with mockito

--- 2.20-starting-project/src/test/java/com/luv2code/test/ApplicationExampleTest.java
+++ 2.20-starting-project/src/test/java/com/luv2code/test/ApplicationExampleTest.java
@@ -1,118 +1,72 @@
 package com.luv2code.test;
 
 import com.luv2code.component.MvcTestingExampleApplication;
+import com.luv2code.component.dao.ApplicationDao;
 import com.luv2code.component.models.CollegeStudent;
 import com.luv2code.component.models.StudentGrades;
+import com.luv2code.component.service.ApplicationService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest(classes= MvcTestingExampleApplication.class)
 public class ApplicationExampleTest {
-
-    private static int count = 0;
-
-    @Value("${info.app.name}")
-    private String appInfo;
-
-    @Value("${info.app.description}")
-    private String appDescription;
-
-    @Value("${info.app.version}")
-    private String appVersion;
-
-    @Value("${info.school.name}")
-    private String schoolName;
-
     @Autowired
-    CollegeStudent student;
-
+    private ApplicationContext applicationContext;
     @Autowired
-    StudentGrades studentGrades;
-
+    private CollegeStudent collegeStudent;
     @Autowired
-    ApplicationContext context;
+    private StudentGrades studentGrades;
+    @MockitoBean
+    private ApplicationDao applicationDao;
+    @Autowired
+    private ApplicationService applicationService;
 
     @BeforeEach
     public void beforeEach() {
-        count = count + 1;
-        System.out.println("Testing: " + appInfo + " which is " + appDescription +
-                "  Version: " + appVersion + ". Execution of test method " + count);
-        student.setFirstname("Eric");
-        student.setLastname("Roby");
-        student.setEmailAddress("eric.roby@luv2code_school.com");
-        studentGrades.setMathGradeResults(new ArrayList<>(Arrays.asList(100.0, 85.0, 76.50, 91.75)));
-        student.setStudentGrades(studentGrades);
+        collegeStudent.setFirstname("Eric");
+        collegeStudent.setLastname("Roby");
+        collegeStudent.setEmailAddress("eric.roby@luv2code_school.com");
+        collegeStudent.setStudentGrades(studentGrades);
     }
-
-    @DisplayName("Add grade results for student grades not equal")
     @Test
-    public void addGradeResultsForStudentGradesAssertNotEquals() {
-        assertNotEquals(0, studentGrades.addGradeResultsForSingleClass(
-                student.getStudentGrades().getMathGradeResults()
-        ));
+    @DisplayName("Test create student")
+    public void createStudent() {
+        when(applicationDao.addGradeResultsForSingleClass(collegeStudent.getStudentGrades().getMathGradeResults())).thenReturn(100.0);
+        assertEquals(100.0, applicationService.addGradeResultsForSingleClass(collegeStudent.getStudentGrades().getMathGradeResults()));
+        verify(applicationDao,times(1)).addGradeResultsForSingleClass(collegeStudent.getStudentGrades().getMathGradeResults());
     }
-
-    @DisplayName("Is grade greater")
     @Test
-    public void isGradeGreaterStudentGrades() {
-        assertTrue(studentGrades.isGradeGreater(90, 75),
-                "failure - should be true");
+    @DisplayName("Test get grade point average")
+    public void getGradePointAverage() {
+        when(applicationDao.findGradePointAverage(collegeStudent.getStudentGrades().getMathGradeResults())).thenReturn(88.31);
+        assertEquals(88.31, applicationService.findGradePointAverage(collegeStudent.getStudentGrades().getMathGradeResults()));
     }
-
-    @DisplayName("Is grade greater false")
+    @DisplayName("Not Null")
     @Test
-    public void isGradeGreaterStudentGradesAssertFalse() {
-        assertFalse(studentGrades.isGradeGreater(89, 92),
-                "failure - should be false");
+    public void notNull() {
+        when(applicationDao.checkNull(collegeStudent)).thenReturn(collegeStudent);
+        assertNotNull(applicationService.checkNull(collegeStudent));
     }
-
-    @DisplayName("Check Null for student grades")
+    @DisplayName("Throw Exception")
     @Test
-    public void checkNullForStudentGrades() {
-        assertNotNull(studentGrades.checkNull(student.getStudentGrades().getMathGradeResults()),
-                "object should not be null");
-    }
-
-    @DisplayName("Create student without grade init")
-    @Test
-    public void createStudentWithoutGradesInit() {
-        CollegeStudent studentTwo = context.getBean("collegeStudent", CollegeStudent.class);
-        studentTwo.setFirstname("Chad");
-        studentTwo.setLastname("Darby");
-        studentTwo.setEmailAddress("chad.darby@luv2code_school.com");
-        assertNotNull(studentTwo.getFirstname());
-        assertNotNull(studentTwo.getLastname());
-        assertNotNull(studentTwo.getEmailAddress());
-        assertNull(studentGrades.checkNull(studentTwo.getStudentGrades()));
-    }
-
-    @DisplayName("Verify students are prototypes")
-    @Test
-    public void verifyStudentsArePrototypes() {
-        CollegeStudent studentTwo = context.getBean("collegeStudent", CollegeStudent.class);
-
-        assertNotSame(student, studentTwo);
-    }
-
-    @DisplayName("Find Grade Point Average")
-    @Test
-    public void findGradePointAverage() {
-        assertAll("Testing all assertEquals",
-                ()-> assertEquals(353.25, studentGrades.addGradeResultsForSingleClass(
-                        student.getStudentGrades().getMathGradeResults())),
-                ()-> assertEquals(88.31, studentGrades.findGradePointAverage(
-                        student.getStudentGrades().getMathGradeResults()))
-        );
+    public void throwException() {
+        when(applicationDao.checkNull(null)).thenThrow(new IllegalArgumentException("Student cannot be null"));
+        assertThrows(IllegalArgumentException.class, () -> applicationService.checkNull(null));
+        assertEquals("Student cannot be null", assertThrows(IllegalArgumentException.class, () -> applicationService.checkNull(null)).getMessage());
     }
 }
 
